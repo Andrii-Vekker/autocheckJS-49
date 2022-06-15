22277dcd7c9b7c0ee05def9b1e21cf46f660fbff
Andrii-Vekker <vekker304@gmail.com>
2022-06-16 01:07:32 +0300

    20/3

--- js/index.js
+++ js/index.js
@@ -3194,21 +3194,24 @@
 // Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
 
 // Change code below this line
-// function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
+//   const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
 
-//   orderedItems.forEach(function (item) {
+//   orderedItems.forEach( item => {
 //     totalPrice += item;
 //   });
 
 //   return totalPrice;
 // }
-const calculateTotalPrice = orderedItems.forEach(number => totalPrice +=number)
 
-console.log(calculateTotalPrice)
- calculateTotalPrice([12, 85, 37, 4])
- calculateTotalPrice([164, 48, 291])
- calculateTotalPrice([412, 371, 94, 63, 176]) 
+  
+  
+  
+  
+// console.log(calculateTotalPrice([12, 85, 37, 4]))
+//  calculateTotalPrice([12, 85, 37, 4])
+//  calculateTotalPrice([164, 48, 291])
+//  calculateTotalPrice([412, 371, 94, 63, 176]) 
 
 ////////////////////////////////////////////////////////////////
 // const products = [
@@ -3237,4 +3240,354 @@ console.log(calculateTotalPrice)
 //   // Change code above this line
 // }
 // getAllPropValues("price")
-///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////task11 ЗАДАЧА. ФИЛЬТРАЦИЯ МАССИВА ЧИСЕЛ 2.0/////////////////////
+// Замени объявление функции filterArray() и коллбек для метода forEach() на стрелочные функции.
+
+// Объявлена переменная filterArray
+// Переменной filterArray присвоена стрелочная функция с параметрами (numbers, value)
+// Для перебора массива numbers использован метод forEach
+// Коллбек для метода forEach это стрелочная функция
+// Вызов функции filterArray([1, 2, 3, 4, 5], 3) возвращает [4, 5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 4) возвращает [5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 5) возвращает []
+// Вызов функции filterArray([12, 24, 8, 41, 76], 38) возвращает [41, 76]
+// Вызов функции filterArray([12, 24, 8, 41, 76], 20) возвращает [24, 41, 76]
+
+// Change code below this line
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach( (number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+
+//  filterArray([1, 2, 3, 4, 5], 3) 
+//  filterArray([1, 2, 3, 4, 5], 4) 
+//  filterArray([1, 2, 3, 4, 5], 5) 
+//  filterArray([12, 24, 8, 41, 76], 38) 
+//  filterArray([12, 24, 8, 41, 76], 20) 
+
+///////////////////////////////////////////////////////////////////task12 ЗАДАЧА. ОБЩИЕ ЭЛЕМЕНТЫ 2.0///////////////////////////////
+// Замени объявление функции getCommonElements() и коллбек для метода forEach() на стрелочные функции.
+// Объявлена переменная getCommonElements.
+// Переменной getCommonElements присвоена стрелочная функция с параметрами (firstArray, secondArray)
+// Для перебора массива firstArray использован метод forEach
+// Коллбек для метода forEach это стрелочная функция
+// Вызов getCommonElements([1, 2, 3], [2, 4]) возвращает [2]
+// Вызов getCommonElements([1, 2, 3], [2, 1, 17, 19]) возвращает [1, 2]
+// Вызов getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) возвращает [12, 27, 3]
+// Вызов getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) возвращает [10, 30, 40]
+// Вызов getCommonElements([1, 2, 3], [10, 20, 30]) возвращает []
+
+// Change code below this line
+// const getCommonElements = (firstArray, secondArray) => {
+//   const commonElements = [];
+
+//   firstArray.forEach((element) => {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+
+//   // Change code above this line
+//   return commonElements;
+// }
+
+//  getCommonElements([1, 2, 3], [2, 4])
+//  getCommonElements([1, 2, 3], [2, 1, 17, 19]) 
+//  getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) 
+//  getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) 
+//  getCommonElements([1, 2, 3], [10, 20, 30]) 
+
+////////////////////////////task 13 ЧИСТЫЕ ФУНКЦИИ/////////////////////////////////////
+// Функция changeEven(numbers, value) принимает массив чисел numbers и обновляет каждый элемент, значение которого это чётное число,
+// добавляя к нему значение параметра value.
+// Выполни рефакторинг функции так, чтобы она стала чистой - не изменяла массив чисел numbers, а создавала, наполняла и возвращала 
+// новый массив с обновлёнными значениями.
+// Объявлена функция changeEven(numbers, value)
+// Функция changeEven не изменяет значение параметра numbers
+// Вызов changeEven([1, 2, 3, 4, 5], 10) возвращает новый массив [1, 12, 3, 14, 5]
+// Вызов changeEven([2, 8, 3, 7, 4, 6], 10) возвращает новый массив [12, 18, 3, 7, 14, 16]
+// Вызов changeEven([17, 24, 68, 31, 42], 100) возвращает новый массив [17, 124, 168, 31, 142]
+// Вызов changeEven([44, 13, 81, 92, 36, 54], 100) возвращает новый массив [144, 13, 81, 192, 136, 154]
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   const newArr = []
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   if (numbers[i] % 2 === 0) {
+//   //     numbers[i] = numbers[i] + value;
+//   //   }
+//   // }
+//   numbers.forEach(number => {
+//     // if (number % 2 === 0) {
+//     //  newArr.push(number + value)
+//     // } else {
+//     //   newArr.push(number)
+//     // }
+//     //console.log(newArr)
+//     ///////declarated/////////////////
+//     //number % 2 === 0 ? newArr.push(number + value) : newArr.push(number)
+//     /////////////////////////////////////
+//   });
+//   console.log(newArr)
+//   //return newArr
+//   // Change code above this line
+// }
+
+//  changeEven([1, 2, 3, 4, 5], 10) 
+//  changeEven([2, 8, 3, 7, 4, 6], 10) 
+//  changeEven([17, 24, 68, 31, 42], 100) 
+// changeEven([44, 13, 81, 92, 36, 54], 100) 
+ 
+//////////////////////////////////task 14 МЕТОД MAP()///////////////////////////////////
+
+// Дополни код так, чтобы в переменной planetsLengths получился массив длин названий планет. Обязательно используй метод map().
+// Объявлена переменная planets
+// Значение переменной planets это массив ["Earth", "Mars", "Venus", "Jupiter"]
+// Объявлена переменная planetsLengths
+// Значение переменной planetsLengths это массив [5, 4, 5, 7]
+// Для перебора массива планет использован метод map()
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// const planetsLengths = planets.map(planet => planet.length);
+
+// console.log(planetsLengths)
+
+///////////////////////////////////////////////task15 МЕТОД MAP() И МАССИВ ОБЪЕКТОВ/////////////////////////
+// Используя метод map() сделай так, чтобы в переменной titles получился массив названий книг (свойство title) из всех объектов массива books.
+// Объявлена переменная books
+// Значение переменной books это массив
+// Объявлена переменная titles
+// Значение переменной titles это массив["The Last Kingdom", "Beside Still Waters", "The Dream of a Ridiculous Man", "Redder Than Blood",
+//   "Enemy of God"]
+// Для перебора массива books используется метод map() как чистая функция
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map(book => book.title);
+// console.log(titles)
+
+///////////////////////////////////////////////////////task16 МЕТОД FLATMAP()/////////////////////
+// Используя метод flatMap() сделай так, чтобы в переменной genres получился массив всех жанров книг (свойство genres) из массива книг books.
+// Объявлена переменная books
+// Значение переменной books это массив объектов
+// Объявлена переменная genres
+// Значение переменной genres это массив [ "adventure", "history", "fiction", "horror", "mysticism" ]
+// Для перебора массива books используется метод flatMap()
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism"],
+//   },
+// ];
+// // Change code below this line
+
+// const genres = books.flatMap(book => book.genres);
+// console.log(genres)
+
+///////////////////////////////////////////////////////task17 ЗАДАЧА. ИМЕНА ПОЛЬЗОВАТЕЛЕЙ//////////////////////////////
+// Дополни функцию getUserNames(users) так, чтобы она возвращала массив имён пользователей (свойство name) из массива объектов в параметре users.
+// Объявлена переменная getUserNames
+// Переменной getUserNames присвоена стрелочная функция с параметром (users).
+// Для перебора параметра users используется метод map()
+// Вызов функции с указанным массивом пользователей возвращает массив["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head",
+//   "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+// const getUserNames = users => {
+    
+// return users.map (user => user.name)
+//   };
+
+//////////////////////////////////task18 массив обьектов пользователей//////////////////////////
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+];
+
+// Дополни функцию getUserEmails(users) так, чтобы она возвращала массив почтовых адресов пользователей(свойство email)
+// из массива объектов в параметре users.
+// Объявлена переменная getUserNames
+// Переменной getUserNames присвоена стрелочная функция с параметром (users)
+// Для перебора параметра users используется метод map()
+// Вызов функции с указанным массивом пользователей возвращает массив["moorehensley@indexia.com", "sharlenebush@tubesys.com",
+// "rossvazquez@xinware.com", "elmahead@omatom.com", "careybarr@nurali.com", "blackburndotson@furnigeer.com", "shereeanthony@kog.com"]
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+// const getUserEmails = users => {
+    
+// return users.map(user => user.email)
+// };
+//   console.log(users.map(user => user.email))
+
+////////////////////////////////////////////task19 МЕТОДЫ FILTER И FIND/////////////////////////////////////////////////
+// Дополни код так, чтобы в переменной evenNumbers получился массив чётных чисел из массива numbers, а в переменной oddNumbers массив нечётных.
+// Обязательно используй метод filter().
+// Объявлена переменная numbers
+// Значение переменной numbers это массив [17, 24, 82, 61, 36, 18, 47, 52, 73]
+// Объявлена переменная evenNumbers
+// Значение переменной evenNumbers это массив [24, 82, 36, 18, 52]
+// Объявлена переменная oddNumbers
+// Значение переменной oddNumbers это массив [17, 61, 47, 73]
+// Для перебора массива numbers использован метод filter()
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(number => {
+//   return number % 2 === 0
+// });
+// const oddNumbers = numbers.filter(number => {
+//   return number % 2 !== 0
+// });
+// console.log(evenNumbers)
+
+////////////////////////////////////////////////////////////task20 ФИЛЬТРАЦИЯ УНИКАЛЬНЫХ ЭЛЕМЕНТОВ///////////////////////////
+// Дополни код так, чтобы в переменной allGenres был массив всех жанров книг(свойство genres) из массива books, а в переменной uniqueGenres
+// массив уникальных жанров - без повторений.
+// Объявлена переменная books
+// Значение переменной books это массив объектов
+// Объявлена переменная allGenres
+// Значение переменной allGenres это массив ["adventure", "history", "fiction", "mysticism", "horror", "mysticism", "adventure"]
+// Объявлена переменная uniqueGenres
+// Значение переменной uniqueGenres это массив ["adventure", "history", "fiction", "mysticism", "horror"]
+// Для вычисления значения переменной allGenders использован метод flatMap()
+// Для вычисления значения переменной uniqueGenres использован метод filter()
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction", "mysticism"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism", "adventure"],
+  },
+];
+// Change code below this line
+const allGenres = books.flatMap(book => book.genres);
+const uniqueGenres = allGenres.filter((unique, i, arr) => arr.indexOf(unique) === i);
+console.log(uniqueGenres)
